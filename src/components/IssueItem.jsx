e37092a989a41f17d398ec2ccc92406ec6ab22f1
Maria <mpfrancog@outlook.com>
2023-12-20 23:19:52 -0500

    Added issues component

---
+++ src/components/IssueItem.jsx
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+import { GoIssueOpened, GoIssueClosed, GoComment } from "react-icons/go";
+import { relativeDate } from "../helpers/relativeDate";
+
+export const IssueItem = ({
+  title,
+  number,
+  assignee,
+  commentCount,
+  createdBy,
+  createdDate,
+  labels,
+  status,
+}) => {
+  return (
+    <li>
+      <div>
+        {status === "done" || status === "cancelled" ? (
+          <GoIssueClosed style={{ color: "red" }} />
+        ) : (
+          <GoIssueOpened style={{ color: "green" }} />
+        )}
+      </div>
+      <div className="issue-content">
+        <span>
+          <Link to={`/issue/${number}`}>{title}</Link>
+          {labels.map((label) => (
+            <span key={label} className={`label red`}>
+              {label}
+            </span>
+          ))}
+        </span>
+        <small>
+          #{number} opened {relativeDate(createdDate)} by {createdBy}
+        </small>
+      </div>
+      {assignee && <div>{assignee}</div>}
+      <span className="comment-count">
+        {commentCount > 0 && (
+          <>
+            <GoComment />
+            {commentCount}
+          </>
+        )}
+      </span>
+    </li>
+  );
+};
--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,14 +1,32 @@
-import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import { IssueItem } from "./IssueItem";
 
 export default function IssuesList() {
+  const { isLoading, data } = useQuery(["issues"], () =>
+    fetch("/api/issues").then((res) => res.json())
+  );
   return (
     <div>
       <h1>Issues List</h1>
-      <ul>
-        <li>
-          <Link to="/issue/1">Issue 1</Link>
-        </li>
-      </ul>
+      {isLoading ? (
+        <p>Loading....</p>
+      ) : (
+        <ul className="issues-list">
+          {data.map((issue) => (
+            <IssueItem
+              key={issue.id}
+              title={issue.title}
+              number={issue.number}
+              assignee={issue.assignee}
+              commentCount={issue.comments.length}
+              createdBy={issue.createdBy}
+              createdDate={issue.createdDate}
+              labels={issue.labels}
+              status={issue.status}
+            />
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
