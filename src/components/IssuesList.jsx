c94c714fbdff6846388d5abe64db994ba0697742
Maria <mpfrancog@outlook.com>
2024-01-16 22:16:36 -0500

    added search funtionality

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,23 +1,62 @@
 import { useQuery } from "react-query";
 import { IssueItem } from "./IssueItem";
+import { useState } from "react";
 
 export default function IssuesList({ labelsData, labels, status }) {
-  const { isLoading, data } = useQuery(["issues", { labels, status }], () => {
-    const statusString = status ? `&status=${status}` : "";
-    const labelsString = labels.map((label) => `labels[]=${label}`).join("&");
-    return fetch(`/api/issues?${labelsString}${statusString}`).then((res) =>
-      res.json()
-    );
-  });
+  const { isLoading: isLoadingIssues, data: issuesData } = useQuery(
+    ["issues", { labels, status }],
+    () => {
+      const statusString = status ? `&status=${status}` : "";
+      const labelsString = labels.map((label) => `labels[]=${label}`).join("&");
+      return fetch(`/api/issues?${labelsString}${statusString}`).then((res) =>
+        res.json()
+      );
+    }
+  );
+
+  const [searchValue, setSearchValue] = useState("");
+
+  const {
+    isLoading: isLoadingSearch,
+    data: searchData,
+    fetchStatus: searchFetchStatus,
+  } = useQuery(
+    ["issues", "search", searchValue],
+    () => {
+      return fetch(`/api/search/issues?q=${searchValue}`).then((res) =>
+        res.json()
+      );
+    },
+    { enabled: !!searchValue }
+  );
 
   return (
     <div>
-      <h1>Issues List</h1>
-      {isLoading ? (
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          setSearchValue(event.target.elements.search.value);
+        }}
+      >
+        <label htmlFor="search">Search Issues</label>
+        <input
+          type="search"
+          placeholder="Search"
+          name="search"
+          id="search"
+          onChange={(event) => {
+            if (event.target.value.length === 0) {
+              setSearchValue("");
+            }
+          }}
+        />
+      </form>
+      <h2>Issues List</h2>
+      {isLoadingIssues ? (
         <p>Loading....</p>
-      ) : (
+      ) : searchFetchStatus === "idle" && isLoadingSearch ? (
         <ul className="issues-list">
-          {data.map((issue) => (
+          {issuesData.map((issue) => (
             <IssueItem
               labelsData={labelsData}
               key={issue.id}
@@ -32,6 +71,33 @@ export default function IssuesList({ labelsData, labels, status }) {
             />
           ))}
         </ul>
+      ) : (
+        <>
+          <h2>Search results</h2>
+          {isLoadingSearch ? (
+            <p>Loading...</p>
+          ) : (
+            <>
+              <p>{searchData.count} Results</p>
+              <ul className="issues-list">
+                {searchData.items.map((issue) => (
+                  <IssueItem
+                    labelsData={labelsData}
+                    key={issue.id}
+                    title={issue.title}
+                    number={issue.number}
+                    assignee={issue.assignee}
+                    commentCount={issue.comments.length}
+                    createdBy={issue.createdBy}
+                    createdDate={issue.createdDate}
+                    labels={issue.labels}
+                    status={issue.status}
+                  />
+                ))}
+              </ul>
+            </>
+          )}
+        </>
       )}
     </div>
   );
--- src/pages/Issues.jsx
+++ src/pages/Issues.jsx
@@ -13,7 +13,6 @@ export default function Issues() {
     <div>
       <main>
         <section>
-          <h1>Issues</h1>
           <IssuesList labelsData={labelsData} labels={labels} status={status} />
         </section>
         <aside>
